7222bcbf1898af5a50ebfe5f2b2aba6adaed8a3c
Maxim Korchagin <pirate227@yandex.ru>
2022-06-09 19:20:23 +0300

    Массив перенесен в отдельный файл

---
+++ blocks/elements/elements.js
@@ -0,0 +1,26 @@
+const elements = [
+  {
+    title: 'Лас-Вегас',
+    link: 'https://images.unsplash.com/photo-1653161652427-135caab0dae1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80',
+  },
+  {
+    title: 'Антарктика',
+    link: 'https://images.unsplash.com/photo-1650359369952-bf80eb7bae5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1013&q=80',
+  },
+  {
+    title: 'Эдинбург',
+    link: 'https://images.unsplash.com/photo-1653315381156-70055ca1df1c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+  },
+  {
+    title: 'Гиза',
+    link: 'https://images.unsplash.com/photo-1640005438758-861043e64aa5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80',
+  },
+  {
+    title: 'Тегеран',
+    link: 'https://images.unsplash.com/photo-1652806724545-669233b0b1f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+  },
+  {
+    title: 'Мачу-Пикчу',
+    link: 'https://images.unsplash.com/photo-1651389331411-bb49cbb9f331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+  },
+];
